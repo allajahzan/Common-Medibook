cbef7ec1201662b5fdcbca1405ec69e760f54dc8
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-24 20:13:08 +0530

    refactor: bcrypt functions

--- src/config/bcrypt.ts
+++ src/config/bcrypt.ts
@@ -4,16 +4,19 @@ export const HashedPassword = async (password: string) => {
     try {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-        return hashedPassword
+        return hashedPassword;
     } catch (err: any) {
         throw new Error(err);
     }
 };
 
-export const VerifyPassword = async (password: string, hashedPassword: string) => {
+export const VerifyPassword = async (
+    password: string,
+    hashedPassword: string
+) => {
     try {
-        return await bcrypt.compare(password, hashedPassword)
+        return await bcrypt.compare(password, hashedPassword);
     } catch (err: any) {
-        throw new Error(err)
+        throw new Error(err);
     }
-}
+};
