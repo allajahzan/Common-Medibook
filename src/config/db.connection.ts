df807ed1252f9e204fa0ebd90f9f1912b7bc7057
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-24 09:53:08 +0530

    patch : throw error message

--- src/config/db.connection.ts
+++ src/config/db.connection.ts
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 export const MongoDBConnection = async (url: string) => {
     try {
-        const resp = await mongoose.connect(url, { connectTimeoutMS: 1000, serverSelectionTimeoutMS: 5000 })
-        return { message: "successfully connected to db", status: true }
-    } catch (err) {
-        return { message: err, status: false }
+        await mongoose.connect(url, { connectTimeoutMS: 1000, serverSelectionTimeoutMS: 5000 })
+        return "successfully connected to db"
+    } catch (err: any) {
+        throw new Error(err.message)
     }
 }
