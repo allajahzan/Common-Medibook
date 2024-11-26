ce1db9ecee259456294a2f2fa7ab49b54317507a
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-26 14:46:04 +0530

    feat: added queue, exchange, key

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ export * from "./errors/error.cutsom";
 export * from "./errors/error.forbidon";
 export * from "./errors/error.notFound";
 export * from "./errors/error.conflict";
-export * from './errors/error.unathorized'
+export * from "./errors/error.unathorized";
 
 //configs
 export * from "./config/db.connection";
-export * from './config/rabbitmq.connection'
+export * from "./config/rabbitmq.connection";
 
 // utils
 export * from "./utils/jwt.tokens";
@@ -20,3 +20,7 @@ export * from "./functions/responses";
 // middlewares
 export * from "./middlewares/error.handler";
 
+// messaging
+export * from "./messaging/exchange";
+export * from "./messaging/queue";
+export * from "./messaging/routingKey";
