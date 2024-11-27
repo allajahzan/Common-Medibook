db8e98b84570a2ccf219c6a8344e01471f4ddc30
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-28 00:20:01 +0530

    patch : added retry mecahnism in db cnctn

--- src/config/db.connection.ts
+++ src/config/db.connection.ts
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 
 export const MongoDBConnection = async (url: string): Promise<void> => {
-    let ATEMPT = 0;
+    let ATTEMPT = 0;
     let RETRIES = 10;
     let DELAY = 5000;
-    let isConnected = false
+    let isConnected = false;
     while (!isConnected) {
         try {
-            await mongoose.connect(url+'k', {
+            await mongoose.connect(url + "k", {
                 connectTimeoutMS: 1000,
                 serverSelectionTimeoutMS: 5000,
             });
             console.log("successfully connected to db");
+            isConnected = true
             break;
         } catch (err: any) {
-            ATEMPT += 1;
+            ATTEMPT += 1;
 
-            if(ATEMPT>RETRIES){
+            console.log(`failed to connect to mongodb retrying in 5s => ${ATTEMPT}th attempt`)
+
+            if (ATTEMPT > RETRIES) {
                 throw new Error(err.message);
             }
 
