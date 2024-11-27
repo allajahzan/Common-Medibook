8795a06c7c719283381d8d4a9cbc65ff7153baa1
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-28 00:01:02 +0530

    patch: retry mechanism in mongoBD

--- src/config/db.connection.ts
+++ src/config/db.connection.ts
@@ -1,13 +1,26 @@
 import mongoose from "mongoose";
 
-export const MongoDBConnection = async (url: string) : Promise<void> => {
-    try {
-        await mongoose.connect(url, {
-            connectTimeoutMS: 1000,
-            serverSelectionTimeoutMS: 5000,
-        });
-        console.log("successfully connected to db")
-    } catch (err: any) {
-        throw new Error(err.message);
+export const MongoDBConnection = async (url: string): Promise<void> => {
+    let ATEMPT = 0;
+    let RETRIES = 10;
+    let DELAY = 5000;
+    let isConnected = false
+    while (!isConnected) {
+        try {
+            await mongoose.connect(url+'k', {
+                connectTimeoutMS: 1000,
+                serverSelectionTimeoutMS: 5000,
+            });
+            console.log("successfully connected to db");
+            break;
+        } catch (err: any) {
+            ATEMPT += 1;
+
+            if(ATEMPT>RETRIES){
+                throw new Error(err.message);
+            }
+
+            await new Promise((res) => setTimeout(res, DELAY));
+        }
     }
 };
