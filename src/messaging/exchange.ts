0b194a4d0edd0d89631fd3f3051885b5c8626ae2
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-29 10:15:06 +0530

    patch: added some queues and exchange

--- src/messaging/exchange.ts
+++ src/messaging/exchange.ts
@@ -2,4 +2,5 @@ export enum Exchanges {
     SIGNUP_EXCHANGE = "user-signup-exchange",
     STATUS_EXCHANGE = "user-status-exchange",
     PROFILE_EXCHANGE = 'doctor-profile-exchange',
+    PROFILE_STATUS = 'doctor-profile-statu-exchange'
 }
--- src/messaging/routingKey.ts
+++ src/messaging/routingKey.ts
@@ -2,5 +2,5 @@ export enum RoutingKey {
     CLIENT_SIGNUP = 'client.signup',
     DOCTOR_SIGNUP = 'doctor.signup',
     DOCTOR_PROFILE_CREATE = 'doctor.profile.create',
-    DOCTOR_PROFILE_UPDATE = 'doctor.profile.update'
+    DOCTOR_PROFILE_UPDATE = 'doctor.profile.update',
 }
