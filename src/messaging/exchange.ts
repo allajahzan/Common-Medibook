3bf1a3c3f8f2dd7c044683206f61314639e523f1
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-11-29 10:28:29 +0530

    patch: removed exchange and added routing key

--- src/messaging/exchange.ts
+++ src/messaging/exchange.ts
@@ -2,5 +2,4 @@ export enum Exchanges {
     SIGNUP_EXCHANGE = "user-signup-exchange",
     STATUS_EXCHANGE = "user-status-exchange",
     PROFILE_EXCHANGE = 'doctor-profile-exchange',
-    PROFILE_STATUS = 'doctor-profile-statu-exchange'
 }
--- src/messaging/routingKey.ts
+++ src/messaging/routingKey.ts
@@ -3,4 +3,6 @@ export enum RoutingKey {
     DOCTOR_SIGNUP = 'doctor.signup',
     DOCTOR_PROFILE_CREATE = 'doctor.profile.create',
     DOCTOR_PROFILE_UPDATE = 'doctor.profile.update',
+    USER_STATUS = 'user.status.change',
+    PROFILE_STATUS = 'doctor.profile.status.change'
 }
